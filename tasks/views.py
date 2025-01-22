e1b57e86f187ce3b378094bfdf65e5f264ec56a5
Pritom Sarkar <pritom.cse.29@gmail.com>
2025-01-23 00:08:10 +0600

    initial project Setup  Done

---
+++ tasks/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    # Work With database
+    # Transform Data
+    # data pass
+    # Http response / Json response
+    return HttpResponse("<h1>Welcome Task Management System</h1>")
+def contact(request):
+    return HttpResponse("<strong style='color:red;'>This is Contact Page<strong>")
+def show_task(request):
+    return HttpResponse("Show Your Task")
